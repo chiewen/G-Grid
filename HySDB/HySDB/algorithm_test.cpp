78e1ad0196a960585eaf8895957fc2a3c25f5c75
chiewen <lichuanwen@mail.neu.edu.cn>
2018-01-28 20:11:44 +0800

    home

--- HySDB/HySDB/algorithm_test.cpp
+++ HySDB/HySDB/algorithm_test.cpp
@@ -10,22 +10,28 @@ TEST(Algorithm, SubnetBfCpu) {
 	CudaGuard guard;
 	Objects::Initialize();
 	int cells[]{0,1,2,3,4,6};
-	SubnetBF::SBfResult result[6 * Index::kMaxVerticesPerCell];
+	const int result_length = 6 * Index::kMaxVerticesPerCell;
+	SubnetBF::SBfResult result[result_length];
 
 	SubnetBF::BfOnCpu(cells, 6, 0, 0, 0, 172, result, Index::grid_[0].edge_num + Index::grid_[1].edge_num + Index::grid_[2].edge_num + Index::grid_[3].edge_num + Index::grid_[4].edge_num + Index::grid_[6].edge_num);
 
-	for (auto& bf_result : result) {
-		if (bf_result.id_ != 0)
-			std::cout << bf_result.id_ << " :" << bf_result.previous_vertex_id << " " << bf_result.distance_ << " " << std::endl;
-	}
-	std::cout << std::endl << std::endl;
+//	for (auto& bf_result : result) {
+//		if (bf_result.id_ != 0)
+//			std::cout << bf_result.id_ << " :" << bf_result.previous_vertex_id << " " << bf_result.distance_ << " " << std::endl;
+//	}
+//	std::cout << std::endl << std::endl;
 
-	SubnetBF::SBfResult result_g[6 * Index::kMaxVerticesPerCell];
+	SubnetBF::SBfResult result_g[result_length];
 	SubnetBF::BfOnGpu(cells, 6, 0, 0, 0, 172, result_g, Index::grid_[0].edge_num + Index::grid_[1].edge_num + Index::grid_[2].edge_num + Index::grid_[3].edge_num + Index::grid_[4].edge_num + Index::grid_[6].edge_num);
 
-	for (auto& bf_result : result_g) {
-		if (bf_result.id_ != 0)
-			std::cout << bf_result.id_ << " :" << bf_result.previous_vertex_id << " " << bf_result.distance_ << " " << std::endl;
+//	for (auto& bf_result : result_g) {
+//		if (bf_result.id_ != 0)
+//			std::cout << bf_result.id_ << " :" << bf_result.previous_vertex_id << " " << bf_result.distance_ << " " << std::endl;
+//	}
+//	std::cout << std::endl << std::endl;
+	for (int i = 0; i < result_length; ++i) {
+		ASSERT_EQ(result[i].distance_, result_g[i].distance_);
+		ASSERT_EQ(result[i].id_, result_g[i].id_);
+		ASSERT_EQ(result[i].previous_vertex_id, result_g[i].previous_vertex_id);
 	}
-	std::cout << std::endl << std::endl;
 }
